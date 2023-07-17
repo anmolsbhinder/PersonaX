5ced45d648c14ce9f8d8633b7ffd00028b1a866f
Anmol Singh <anmolsbhinder@gmail.com>
2023-07-17 16:10:15 +0530

    removed comments

--- SCList.py
+++ SCList.py
@@ -12,18 +12,8 @@ Scriterias = pd.read_csv('/database/segment_master.csv')
 @app.route('/api/segmentpg', methods=['GET'])
 def get_segments_criterias():
 
-    # df1 = pd.DataFrame(criteria)
-    # df1 = df1.drop(columns=['CustomerID'])
-    # criterialist = df1.tolist()
-    # print(df1)
-
     df2 = pd.DataFrame(Scriterias)
     segmentlist = df2['Segment Name'].tolist()
-
-    # response = {
-    #     'criterialist': df1,
-    #     'segmentlist': segmentlist
-    # }
 
     return jsonify(segmentlist) , 200, {'Content-Type': 'application/json'}
 
